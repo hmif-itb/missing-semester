6acbda81a175579b6ed22145d72e32655fed4088
KenEzekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-12-05 01:55:39 +0700

    add: initial setup for the missing semester

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,62 +1,60 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "The Missing Semester",
+  tagline: "Mewujudkan Nirmana Akselerasi Potensi HMIF ITB",
+  favicon: "img/Logo HMIF SVG.svg",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "HMIF ITB",
+  projectName: "missing-semester",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -64,72 +62,69 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/Logo HMIF SVG.png",
     navbar: {
-      title: 'My Site',
+      title: "The Missing Semester",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "HMIF Logo",
+        src: "img/Logo HMIF SVG.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/hmif-itb",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          href: "https://www.instagram.com/hmif_itb/",
+          label: "Instagram",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Tutorial",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "HMIF App",
+              href: "https://app.hmif.dev",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "GitHub",
+              href: "https://github.com/hmif-itb",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} HMIF Missing Semester`,
     },
     prism: {
       theme: prismThemes.github,
---
+++ src/pages/home.md
@@ -0,0 +1,11 @@
+---
+title: Home
+author: Kenneth Ezekiel
+date: 5-12-2024 1:55
+---
+
+# The Missing Semester by HMIF ITB
+
+The Missing Semester atau dapat disebut juga Kitab Suci HMIF (HMIF Knowledge Base) adalah sebuah koleksi dari dokumen dengan berbagai tema, yang ditulis untuk dipelajari dan juga berbagi ilmu.
+
+Project ini dibuat pada tahun 2024, pada kepengurusan Dewan Eksekutif HMIF ITB 2024/2025, dengan nama HMIF Nirmana. Sesuai dengan visi HMIF Nirmana, yaitu "Mewujudkan Nirmana Akselerasi Potensi HMIF ITB", dimana Nirmana berarti "kumpulan unsur-unsur yang disusun secara harmonis", visi ini berarti bahwa HMIF ITB harus dapat disusun dengan sesuai, menjadi wadah yang sesuai dengan isinya, yaitu mahasiswa, dan menjadi sebuah organisasi yang relevan dengan mahasiswanya, yang akan membuat pencapaian potensi bagi mahasiswa HMIF ITB terakselerasi. Project ini menjadi sebuah inisiatif yang diharapkan membantu pencapaian potensi bagi warga HMIF ITB, dengan dapat berbagi ilmu, terus belajar, dan juga dapat menunjukkan relevansi dan eksistensi HMIF ITB di berbagai struktur sosial.
